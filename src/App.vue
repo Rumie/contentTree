2db35d3f4531b76ca2643ae3c767a953c8b6c08c
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2020-02-23 13:27:57 +0530

    improve look and feel of the explorer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <el-input placeholder="Filter keyword" v-model="filterText" style="margin-bottom: 20px;"></el-input>
+    <!-- nav header  -->
+    <nav class="navigation">
+      <img src="@/assets/images/logo.svg" alt="Rumie" />
+      <div class="navigation__title">
+        Rumie Content Categories
+      </div>
+    </nav>
+
+
+
+    <!-- search -->
+    <div style="margin: 10px">
+      <el-input placeholder="Filter keyword" v-model="filterText" style="margin-bottom: 20px;"></el-input>
+    </div>
+
+    <!-- tree explorer -->
+    <!-- TODO: migrate to it's own component, ready for export and print -->
     <el-tree
       default-expand-all
       accordion
@@ -41,6 +57,37 @@ export default {
 <style lang="scss">
 body {
   font-family: sans;
-  /* background: #fafafa; */
+}
+
+.navigation {
+  padding: var(--spacing-4);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.navigation__title {
+  margin-top: var(--spacing-2);
+  font-weight: var(--bold);
+  font-size: var(--xl1);
+  letter-spacing: var(--tracking-tight);
+  margin-left: auto;
+  margin-right: auto;
+}
+
+/* tree style */
+.el-tree-node__content {
+  padding: 5px;
+}
+
+.el-tree-node__content:hover {
+  background: var(--primary-lightest);
+  background-color: var(--primary-lightest);
+}
+
+.el-tree-node.is-current > .el-tree-node__content {
+  background: var(--primary-lightest) !important;
+  background-color: var(--primary-lightest) !important;
 }
 </style>
