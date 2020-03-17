eb68c7f3c4ed659e7a74e8d029afba405ce9cc3f
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2020-03-17 14:37:34 +0530

    add sample component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import App from './App.vue';
+import Sample from './sample.vue';
 
 export default {
 	install: function (Vue) {
 		Vue.component('content-tree', App);
+		Vue.component('sample', Sample);
 	}
 }
