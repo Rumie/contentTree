c42e72a8d4d300174499068a44c80c0f2e3f3b44
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2020-03-17 14:45:29 +0530

    remove app to test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import App from './App.vue';
 import Sample from './sample.vue';
 
 export default {
 	install: function (Vue) {
-		Vue.component('content-tree', App);
 		Vue.component('sample', Sample);
 	}
 }
