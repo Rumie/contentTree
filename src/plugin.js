c3f4d9d1045e9f850302840d22d927d7b0833f08
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2020-04-20 16:33:50 +0530

    attempt to fix missing deps in content tree

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,13 @@
+import "vue-slider-component/theme/default.css";
+import { Input, Tree } from "element-ui";
+import VueSlider from "vue-slider-component";
 import App from './App.vue';
 
 export default {
   install(Vue) {
+    Vue.component("content-tree-slider", VueSlider);
+    Vue.component(Input.name, Input);
+    Vue.component(Tree.name, Tree);
     Vue.component("rum-content-tree", App);
   }
 }
